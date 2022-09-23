e03c346bc567a399ce3dbf426da70e81ffb81323
Joel <joelandriambola@gmail.com>
2022-09-23 14:00:03 +0300

    Dernière modification sur l'index et sur les users

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React , { Component, useState, useEffect } from 'react'
 
 import Table from '../components/table/Table'
 
+
+
 import customerList from '../assets/JsonData/customers-list.json'
+import UserService from '../'
+
 
 const customerTableHead = [
+    'id',
     '',
-    'name',
-    'email',
-    'phone',
-    'total orders',
-    'total spend',
-    'location'
+    'Nom',
+    'Prenom',
+    'Mail',
+    'Genre',
+    'Contact',
+    'Adresse',
+    'Centre d \'intérets',
+    'Evaluation'
 ]
 
 const renderHead = (item, index) => <th key={index}>{item}</th>
@@ -19,20 +26,35 @@ const renderHead = (item, index) => <th key={index}>{item}</th>
 const renderBody = (item, index) => (
     <tr key={index}>
         <td>{item.id}</td>
-        <td>{item.name}</td>
+        <td>{item.photo}</td>
+        <td>{item.nom}</td>
+        <td>{item.prenom}</td>
         <td>{item.email}</td>
-        <td>{item.phone}</td>
-        <td>{item.total_orders}</td>
-        <td>{item.total_spend}</td>
-        <td>{item.location}</td>
+        <td>{item.password}</td>
+        <td>{item.contact}</td>
+        <td>{item.date_naissance}</td>
+        <td>{item.interests}</td>
+        <td>{item.vehicules}</td>
+        <td>{item.evaluation}</td>
     </tr>
 )
 
 const Customers = () => {
+  
+const [users, setUsers] = useState([]);
+
+    
+    useEffect(() => {
+        const logInterest = async () => {
+          const resp = await InterestService.getInterest();
+          setUsers(resp.data);
+        };
+        logInterest();
+      }, []);
     return (
         <div>
             <h2 className="page-header">
-                customers
+                Utilisateurs
             </h2>
             <div className="row">
                 <div className="col-12">
