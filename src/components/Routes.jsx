d84aa6e79e74e7f0d71dc4de08dd16e82d29c69a
Joel <joelandriambola@gmail.com>
2022-09-26 01:34:25 +0300

    Alefa dada

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -6,6 +6,7 @@ import Dashboard from '../pages/Dashboard'
 import Customers from '../pages/Customers'
 import EditUser from './edit/EditUser'
 import UserProfile from '../pages/UserProfile'
+import Products from '../pages/ListEvent'
 
 
 
@@ -16,6 +17,8 @@ const Routes = () => {
             <Route path='/customers' component={Customers}/>
             <Route path='/edit/:idUser' component = {EditUser}/>
             <Route path='/userProfile' component = {UserProfile}/>
+            <Route path='/products' component = {Products}/>
+            
 
         </Switch>
     )
