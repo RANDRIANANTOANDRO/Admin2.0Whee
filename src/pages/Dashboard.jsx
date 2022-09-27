ce1280ac7ddc12a2821a1bce09089c5fb50cbe46
Tsiry <thajavola.gmail.com>
2022-09-27 09:01:13 +0300

    Derniere Mofidication du user

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,246 +1,245 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import Chart from 'react-apexcharts'
+import Chart from "react-apexcharts";
 
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
-import StatusCard from '../components/status-card/StatusCard'
+import StatusCard from "../components/status-card/StatusCard";
 
-import Table from '../components/table/Table'
+import Table from "../components/table/Table";
 
-import Badge from '../components/badge/Badge'
+import Badge from "../components/badge/Badge";
 
-import statusCards from '../assets/JsonData/status-card-data.json'
+import statusCards from "../assets/JsonData/status-card-data.json";
 
 const chartOptions = {
-    series: [{
-        name: 'Covoitureurs',
-        data: [12,23,20,40,45,47,43,60,75]
-    }, {
-        name: 'Conducteurs de Véhicules',
-        data: [11, 15, 17, 20, 25, 30, 35, 40, 50, 55]
-    }],
-    options: {
-        color: ['#6ab04c', '#2980b9'],
-        chart: {
-            background: 'transparent'
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            curve: 'smooth'
-        },
-        xaxis: {
-            categories: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep']
-        },
-        legend: {
-            position: 'top'
-        },
-        grid: {
-            show: false
-        }
-    }
-}
+  series: [
+    {
+      name: "Covoitureurs",
+      data: [12, 23, 20, 40, 45, 47, 43, 60, 75],
+    },
+    {
+      name: "Conducteurs de Véhicules",
+      data: [11, 15, 17, 20, 25, 30, 35, 40, 50, 55],
+    },
+  ],
+  options: {
+    color: ["#6ab04c", "#2980b9"],
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Avr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Aou",
+        "Sep",
+      ],
+    },
+    legend: {
+      position: "top",
+    },
+    grid: {
+      show: false,
+    },
+  },
+};
 
 const topCustomers = {
-    head: [
-        'Utilisateur',
-        'Nombre de Covoiturage',
-        'Evaluation'
-    ],
-    body: [
-        {
-            "username": "john doe",
-            "order": "490",
-            "price": "Ar 15,870"
-        },
-        {
-            "username": "frank iva",
-            "order": "250",
-            
-        },
-        {
-            "username": "anthony baker",
-            "order": "120",
-            "price": "Ar 10,840"
-        },
-        {
-            "username": "frank iva",
-            "order": "110",
-            "price": "Ar 9,251"
-        },
-        {
-            "username": "anthony baker",
-            "order": "80",
-            "price": "Ar 8,840"
-        }
-    ]
-}
+  head: ["Utilisateur", "Nombre de Covoiturage", "Evaluation"],
+  body: [
+    {
+      username: "john doe",
+      order: "490",
+      price: "Ar 15,870",
+    },
+    {
+      username: "frank iva",
+      order: "250",
+    },
+    {
+      username: "anthony baker",
+      order: "120",
+      price: "Ar 10,840",
+    },
+    {
+      username: "frank iva",
+      order: "110",
+      price: "Ar 9,251",
+    },
+    {
+      username: "anthony baker",
+      order: "80",
+      price: "Ar 8,840",
+    },
+  ],
+};
 
-const renderCusomerHead = (item, index) => (
-    <th key={index}>{item}</th>
-)
+const renderCusomerHead = (item, index) => <th key={index}>{item}</th>;
 
 const renderCusomerBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.username}</td>
-        <td>{item.order}</td>
-        <td>{item.price}</td>
-    </tr>
-)
+  <tr key={index}>
+    <td>{item.username}</td>
+    <td>{item.order}</td>
+    <td>{item.price}</td>
+  </tr>
+);
 
 const latestOrders = {
-    header: [
-        "Id",
-        "Nom",
-        "Date D'inscription",
-        "Heure",
-        "Statut "
-    ],
-    body: [
-        {
-            id: "#OD1711",
-            user: "john doe",
-            date: "17 Jun 2021",
-            price: "$900",
-            status: "shipping"
-        },
-        {
-            id: "#OD1712",
-            user: "frank iva",
-            date: "1 Jun 2021",
-            price: "$400",
-            status: "paid"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "pending"
-        },
-        {
-            id: "#OD1712",
-            user: "frank iva",
-            date: "1 Jun 2021",
-            price: "$400",
-            status: "paid"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund"
-        }
-    ]
-}
+  header: ["Id", "Nom", "Date D'inscription", "Heure", "Statut "],
+  body: [
+    {
+      id: "#OD1711",
+      user: "john doe",
+      date: "17 Jun 2021",
+      price: "$900",
+      status: "shipping",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "paid",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "pending",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "paid",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "refund",
+    },
+  ],
+};
 
 const orderStatus = {
-    "Suspendu": "danger",
-    "Validé": "success",
-    "Validé": "success",
-    "Validé": "danger"
-}
+  Suspendu: "danger",
+  Validé: "success",
+  Validé: "success",
+  Validé: "danger",
+};
 
-const renderOrderHead = (item, index) => (
-    <th key={index}>{item}</th>
-)
+const renderOrderHead = (item, index) => <th key={index}>{item}</th>;
 
 const renderOrderBody = (item, index) => (
-    <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.user}</td>
-        <td>{item.price}</td>
-        <td>{item.date}</td>
-        <td>
-            <Badge type={orderStatus[item.status]} content={item.status}/>
-        </td>
-    </tr>
-)
+  <tr key={index}>
+    <td>{item.id}</td>
+    <td>{item.user}</td>
+    <td>{item.price}</td>
+    <td>{item.date}</td>
+    <td>
+      <Badge type={orderStatus[item.status]} content={item.status} />
+    </td>
+  </tr>
+);
 
 const Dashboard = () => {
+  const themeReducer = useSelector((state) => state.ThemeReducer.mode);
 
-    const themeReducer = useSelector(state => state.ThemeReducer.mode)
-
-    return (
-        <div>
-            <h2 className="page-header">Dashboard</h2>
-            <div className="row">
-                <div className="col-6">
-                    <div className="row">
-                        {
-                            statusCards.map((item, index) => (
-                                <div className="col-6" key={index}>
-                                    <StatusCard
-                                        icon={item.icon}
-                                        count={item.count}
-                                        title={item.title}
-                                    />
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-                <div className="col-6">
-                    <div className="card full-height">
-                        {/* chart */}
-                        <Chart
-                            options={themeReducer === 'theme-mode-dark' ? {
-                                ...chartOptions.options,
-                                theme: { mode: 'dark'}
-                            } : {
-                                ...chartOptions.options,
-                                theme: { mode: 'light'}
-                            }}
-                            series={chartOptions.series}
-                            type='line'
-                            height='100%'
-                        />
-                    </div>
-                </div>
-                <div className="col-4">
-                    <div className="card">
-                        <div className="card__header">
-                            <h3>Meilleurs covoitureurs</h3>
-                        </div>
-                        <div className="card__body">
-                            <Table
-                                headData={topCustomers.head}
-                                renderHead={(item, index) => renderCusomerHead(item, index)}
-                                bodyData={topCustomers.body}
-                                renderBody={(item, index) => renderCusomerBody(item, index)}
-                            />
-                        </div>
-                        <div className="card__footer">
-                            <Link to='/'>Voir Tout</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-8">
-                    <div className="card">
-                        <div className="card__header">
-                            <h3>Dernière  </h3>
-                        </div>
-                        <div className="card__body">
-                            <Table
-                                headData={latestOrders.header}
-                                renderHead={(item, index) => renderOrderHead(item, index)}
-                                bodyData={latestOrders.body}
-                                renderBody={(item, index) => renderOrderBody(item, index)}
-                            />
-                        </div>
-                        <div className="card__footer">
-                            <Link to='/'>Voir Tout</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {statusCards.map((item, index) => (
+              <div className="col-6" key={index}>
+                <StatusCard
+                  icon={item.icon}
+                  count={item.count}
+                  title={item.title}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
+        <div className="col-6">
+          <div className="card full-height">
+            {/* chart */}
+            <Chart
+              options={
+                themeReducer === "theme-mode-dark"
+                  ? {
+                      ...chartOptions.options,
+                      theme: { mode: "dark" },
+                    }
+                  : {
+                      ...chartOptions.options,
+                      theme: { mode: "light" },
+                    }
+              }
+              series={chartOptions.series}
+              type="line"
+              height="100%"
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>Meilleurs covoitureurs</h3>
+            </div>
+            <div className="card__body">
+              <Table
+                headData={topCustomers.head}
+                renderHead={(item, index) => renderCusomerHead(item, index)}
+                bodyData={topCustomers.body}
+                renderBody={(item, index) => renderCusomerBody(item, index)}
+              />
+            </div>
+            <div className="card__footer">
+              <Link to="/">Voir Tout</Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="card">
+            <div className="card__header">
+              <h3>Dernières Inscriptions </h3>
+            </div>
+            <div className="card__body">
+              <Table
+                headData={latestOrders.header}
+                renderHead={(item, index) => renderOrderHead(item, index)}
+                bodyData={latestOrders.body}
+                renderBody={(item, index) => renderOrderBody(item, index)}
+              />
+            </div>
+            <div className="card__footer">
+              <Link to="/">Voir Tout</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
